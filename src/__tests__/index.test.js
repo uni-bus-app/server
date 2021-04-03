84c16a984aff2fa308871afea2f69bd83aa78297
Jools Hattey <jools.hg@gmail.com>
2021-04-03 12:33:17 +0100

    added tests for sync route

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,7 +3,8 @@
 const path = require('path');
 const requireDir = require('require-directory');
 const Ajv = require('ajv');
-const { getStops, getTimes, getRoutes } = require('./helpers');
+const { getStops, getTimes, getRoutes, sync } = require('./helpers');
+const hash = require('object-hash');
 
 const schemaURI = 'https://unib.us/schemas';
 const schemas = requireDir(module, path.join(__dirname, 'schemas'));
@@ -42,4 +43,35 @@ test('Routes test', async () => {
     const valid = validate(element);
     expect(valid).toBe(true);
   });
+});
+
+test('Sync test', async () => {
+  // Test sync works with incorrect/missing version
+  const data = await sync({
+    stopsVersion: 'incorrect',
+    timesVersion: 'incorrect',
+  });
+  expect(data.updates).toBe(true);
+  expect(data.stops).toBeTruthy();
+  expect(data.times).toBeTruthy();
+  data.stops.forEach((element) => {
+    const validate = ajv.getSchema(`${schemaURI}/stop`);
+    const valid = validate(element);
+    expect(valid).toBe(true);
+  });
+  data.times.forEach((element) => {
+    element.times.forEach((time) => {
+      const validate = ajv.getSchema(`${schemaURI}/time`);
+      const valid = validate(time);
+      expect(valid).toBe(true);
+    });
+  });
+
+  // Generate correct version from times/stops data
+  const stopsVersion = hash(data.stops, { respectType: false });
+  const timesVersion = hash(data.times, { unorderedArrays: true });
+
+  // Test sync route returns no updates if correct versions are sent
+  const data1 = await sync({ stopsVersion, timesVersion });
+  expect(data1.updates).toBe(false);
 });
